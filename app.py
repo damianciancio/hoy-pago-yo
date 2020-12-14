4ade6ec2dba2e6c44199ff4b71614ae14222ca3a
Damián Ciancio <damianciancio7@gmail.com>
2020-12-14 19:34:58 -0300

    Agrego config de la bd remota-

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'asddq132324{{´+'
+on_heroku = False
+if 'HEROKU_APP' in os.environ:
+    on_heroku = True
+
+if on_heroku:
+    db_url = 'mysql+mysqlconnector://Edf63AUxmZ:0fpeIbtgvL@remotemysql.com/Edf63AUxmZ'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://mywebuser:web000@localhost/hoy_pago_yo'
 
 db=SQLAlchemy(app)
